8bbbc6fdb9cb521bf5983cf398c8702fe2f6d104
alex <alexabolot@gmail.com>
2022-04-23 22:32:19 +0500

    Вынес меню в отдельный шаблон, оформил динамику в шаблоны

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', mainapp.main, name='index'),
     path('products', mainapp.products, name='products'),
     path('contacts', mainapp.contacts, name='contacts'),
-    path('product_details/<int:pk>', mainapp.product, name='product')
+    # path('product_details/<int:pk>', mainapp.product, name='product')
 ]
