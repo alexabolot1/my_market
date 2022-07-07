c3f2820f6f9b8831a28c2865a74436a0f42c0995
alex <alexabolot@gmail.com>
2022-07-07 16:49:53 +0500

    Добавление пагинации на странице продуктов, реализация отображения продуктов с активной категорией

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 import random
 
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
@@ -8,12 +9,17 @@ def get_category_menu():
     return Category.objects.filter(is_active=True)
 
 
+def get_product_active_category():
+    return Product.objects.filter(category__is_active=True)
+
+
 def get_hot_product():
-    return random.choice(Product.objects.all())
+    return random.choice(get_product_active_category())
 
 
 def get_same_products(hot_product):
-    return Product.objects.filter(category=hot_product.category).exclude(id=hot_product.id)[:3]
+    return Product.objects.filter(category=hot_product.category,
+                                  category__is_active=True).exclude(id=hot_product.id)[:3]
 
 
 def index(request):
@@ -60,15 +66,18 @@ def products(request):
 def category(request, pk):
     if pk == 0:
         cat = {'pk': 0, 'name': 'все'}
-        prod = Product.objects.all()
+        prod = get_product_active_category()
     else:
         cat = get_object_or_404(Category, pk=pk)
         prod = Product.objects.filter(category=cat)
+    paginator = Paginator(prod, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'title': 'товары категории',
         'categories': Category.objects.all(),
         'category': cat,
-        'products': prod,
+        'page_obj': page_obj,
     }
     return render(request, 'mainapp/category_products.html', context)
 
